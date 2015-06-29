6f51099239ab1cb90f60d41fe23379bdfa8793a3
Mihai Ionescu <intellidream@live.com>
2015-06-29 23:38:11 +0300

    Restarted implementation of data contracts.

---
+++ Blink.Data/Blink.Data.Contracts/Note.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blink.Data.Contracts
+{
+    public class Folder
+    {
+        public string Hash { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+
+        //public string Hash()
+        //{
+        //    return new MD5CryptoProvider()
+        //}
+    }
+
+
+    public class Note
+    {
+        public Guid NoteId { get; set; }
+        public Guid ParentId { get; set; }
+
+    }
+
+    public class NoteValue
+    {
+
+    }
+}
