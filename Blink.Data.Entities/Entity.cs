3574769f6668725467da692bb593d08d8e1f112b
Mihai Ionescu <intellidream@live.com>
2016-08-22 01:28:11 +0300

    Added Entities project.

---
+++ Blink.Data.Entities/Entity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blink.Data.Entities
+{
+    public interface IEntity
+    {
+        Guid Id { get; set; }
+        Timestamp Timestamp { get; set; }
+    }
+
+    public struct Timestamp
+    {
+        public DateTimeOffset? Created { get; set; }
+        public DateTimeOffset? Updated { get; set; }
+    }
+}
---
+++ Blink.Data.Entities/Notes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blink.Data.Entities
+{
+    public class Note : IEntity
+    {
+        public Guid Id { get; set; }
+        public Timestamp Timestamp { get; set; }
+
+        public Guid UserId { get; set; }
+        public Guid? CategoryId { get; set; }
+        public Guid? ContentId { get; set; }
+        public int? Position { get; set; }
+    }
+
+    public class TextContent : IEntity
+    {
+        public Guid Id { get; set; }
+        public Timestamp Timestamp { get; set; }
+
+        public string Value { get; set; }
+    }
+}
